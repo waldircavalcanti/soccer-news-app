0e391863f94aa06f32683036cc0aff4238c33030
Waldir Cavalcanti <walcanty22@gmail.com>
2022-06-09 11:08:58 -0300

    Inclusão do Retrofit e Picasso

--- app/src/main/java/me/dio/soccernews/ui/adapters/NewsAdapter.java
+++ app/src/main/java/me/dio/soccernews/ui/adapters/NewsAdapter.java
@@ -1,10 +1,14 @@
 package me.dio.soccernews.ui.adapters;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -16,7 +20,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     private List<News> news;
 
-    public NewsAdapter(List<News> news){
+    public NewsAdapter(List<News> news) {
         this.news = news;
     }
 
@@ -25,7 +29,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        NewsItemBinding binding = NewsItemBinding.inflate(layoutInflater,parent,false);
+        NewsItemBinding binding = NewsItemBinding.inflate(layoutInflater, parent, false);
         return new ViewHolder(binding);
     }
 
@@ -34,6 +38,12 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         News news = this.news.get(position);
         holder.binding.tvTitle.setText(news.getTitle());
         holder.binding.tvDescription.setText(news.getDescription());
+        Picasso.get().load(news.getImage()).fit().into(holder.binding.ivThumbnail);
+        holder.binding.btOpenLink.setOnClickListener(view ->{
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(news.getLink()));
+            holder.itemView.getContext().startActivity(i);
+        });
 
     }
 
@@ -42,11 +52,11 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         return this.news.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public final NewsItemBinding binding;
 
-        public ViewHolder(NewsItemBinding binding){
+        public ViewHolder(NewsItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -4,28 +4,51 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public final class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
 
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://waldircavalcanti.github.io/soccer-news-app-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        //TODO Remover Mock
-        List<News> news = new ArrayList<>();
-        news.add(new News("Sport Campeão","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"));
-        news.add(new News("Sport joga domingo","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"));
-        news.add(new News("Copa do  mundo Volei ","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"));
+         api = retrofit.create(SoccerNewsApi.class);
 
-        this.news.setValue(news);
 
+        this.findNews();
+    }
+
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()){
+                    news.setValue(response.body());
+                }else {
+                    //TODO tratamento de erros
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                //TODO tratamento de erros
+            }
+        });
     }
 
     public LiveData<List<News>> getNews(){
